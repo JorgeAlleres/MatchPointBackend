4f856eef78eeeb3000a233db5a0e60cfd9b749d4
JorgeAlleres <alleresjorge10@gmail.com>
2025-01-23 13:48:06 +0100

    Inicio Proyecto

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, {Response, Request} from 'express'
+import authRouter from '@/routes/auth.routes'
+import userRouter from '@/routes/user.routes'
+
+const app = express()
+
+app.use(express.json())
+
+app.use('/api/auth',authRouter)
+app.use('/api/user',userRouter)
+
+app.get('/', (req:Request, res:Response)=>{
+    res.send('Bienvenido al backend (PLAN IT)')
+})
+
+export default app
