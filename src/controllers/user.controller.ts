4c85b3b91da8da0039cd995d251f09229e9aee64
JorgeAlleres <alleresjorge10@gmail.com>
2025-01-29 20:58:37 +0100

    Añado funcionalidad para listar usuarios

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,18 @@
-import { UserService } from "@/services/user.service";
-import {Response, Request} from 'express'
+import { UserService } from "../services/user.service";
+import {Response, Request, NextFunction} from 'express'
 
 export class UserController{
     static async profile(req:Request, res:Response) {
         const email = req.body.user.email
         const user =  await UserService.getByEmail(email)
         res.status(200).json(user)
+    }
+    static async usersList(req:Request, res:Response, next:NextFunction) {
+        try {
+            const users = await UserService.getAll()
+            res.status(200).json(users)
+        } catch(error) {
+            next(error)
+        }
     }
 }
