6dfd91972b35e6b08d5d2a84dead07ac4b97c540
JorgeAlleres <alleresjorge10@gmail.com>
2025-01-29 20:57:44 +0100

    Corrijo importes y añado rutas

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import {UserController} from '@/controllers/user.controller'
-import { isAuthenticate } from "@/middlewares/auth.middleware";
+import {UserController} from '../controllers/user.controller'
+import { isAuthenticate } from "../middlewares/auth.middleware";
+import { isAdmin } from "../middlewares/user.middleware";
 
 const router = Router()
 
+router.get('/', isAuthenticate, isAdmin, UserController.usersList)
 router.get('/profile', isAuthenticate, UserController.profile)
 
 export default router
