ac0ed440cb3cab706dd3c573e035abe24482a02f
JorgeAlleres <alleresjorge10@gmail.com>
2025-02-07 14:11:27 +0100

    validaciones

--- src/middlewares/validators.middleware.ts
+++ src/middlewares/validators.middleware.ts
@@ -2,13 +2,13 @@ import { body } from "express-validator"
 
 export const registerValidation = [
     body('email').isEmail().withMessage('Invalid Email'),
-    body('password').isStrongPassword().withMessage('Password too short'),
+    body('password').isStrongPassword().withMessage('Password too weak'),
     body('userName').notEmpty().withMessage('User Name required'),
 
 ]
 
 export const loginValidation = [
     body('email').isEmail().withMessage('Invalid Email'),
-    body('password').isStrongPassword().withMessage('Password too short')
+    body('password').isStrongPassword().withMessage('Password too weak')
     
 ]
