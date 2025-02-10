7ed2ff7a35f06cf8c1112c12fdb4466d944567f8
JorgeAlleres <alleresjorge10@gmail.com>
2025-02-10 13:32:43 +0100

    Arreglo delete y update de rooms

--- src/routes/room.routes.ts
+++ src/routes/room.routes.ts
@@ -7,7 +7,7 @@ const router = Router()
 router.post('/',isAuthenticate, RoomController.create)
 router.get('/:id',isAuthenticate, RoomController.getById)
 router.get('/',isAuthenticate, RoomController.getAll)
-router.put('/:id',isAuthenticate, RoomController.update) //TODO No funciona
-router.delete('/:id',isAuthenticate, RoomController.delete) //TODO No funciona
+router.put('/:id',isAuthenticate, RoomController.update)
+router.delete('/:id',isAuthenticate, RoomController.delete)
 
 export default router
