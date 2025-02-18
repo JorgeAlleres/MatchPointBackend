c912e131400b5684cca5ba53441b2d2add4b3cb0
JorgeAlleres <alleresjorge10@gmail.com>
2025-02-18 22:27:14 +0100

    Añago getById y getByGameId

--- src/controllers/room.controller.ts
+++ src/controllers/room.controller.ts
@@ -3,18 +3,31 @@ import { RoomService } from "../services/room.service"
 import { NextFunction, Request, Response } from "express"
 
 export class RoomController {
-    static async getById(req:Request, res:Response, next:NextFunction){
-        try{
+    static async getById(req: Request, res: Response, next: NextFunction) {
+        try {
             const id = Number.parseInt(req.params.id)
             if (isNaN(id)) throw new HttpException(400, "Invalid room ID");
 
             // pasar a entero
             const room = await RoomService.getById(id)
             res.status(200).json(room)
-        }catch(error){
+        } catch (error) {
             next(error)
         }
     }
+    /* Función para obtener las salas filtradas por 'gameId'
+    static async getRoomsByGameId(req: Request, res: Response): Promise<void> {
+        const { gameId } = req.params; // Extraemos 'gameId' de los parámetros de la URL
+
+        try {
+            // Llamamos al servicio para obtener las salas relacionadas con el 'gameId'
+            const rooms = await RoomService.getRoomsByGameId(Number(gameId));
+            res.json(rooms);  // Enviamos la respuesta con las salas encontradas
+        } catch (error) {
+            // Si ocurre un error, respondemos con un mensaje de error
+            res.status(500).json({ message: 'Error al obtener las salas', error: error instanceof Error ? error.message : 'Desconocido' });
+        }
+    }*/
 
     static async getAll(req: Request, res: Response, next: NextFunction) {
         try {
@@ -25,7 +38,7 @@ export class RoomController {
             next(error)
         }
     }
-    static async create(req: Request, res: Response, next:NextFunction) {
+    static async create(req: Request, res: Response, next: NextFunction) {
         try {
             const roomData = req.body
             const userId = req.user?.id
@@ -36,7 +49,7 @@ export class RoomController {
             next(error)
         }
     }
-    static async delete(req:Request, res:Response, next:NextFunction) {
+    static async delete(req: Request, res: Response, next: NextFunction) {
         try {
             const id = Number.parseInt(req.params.id)
             if (isNaN(id)) throw new HttpException(400, "Invalid room ID");
@@ -47,7 +60,7 @@ export class RoomController {
             next(error)
         }
     }
-    static async update(req: Request, res: Response, next:NextFunction) {
+    static async update(req: Request, res: Response, next: NextFunction) {
         try {
             const roomData = req.body
             const id = Number.parseInt(req.params.id)
