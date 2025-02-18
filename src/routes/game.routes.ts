0f0affab8f1cb5e8d84c5ce394cc830832efa186
JorgeAlleres <alleresjorge10@gmail.com>
2025-02-18 22:27:44 +0100

    Corrijo credenciales

--- src/routes/game.routes.ts
+++ src/routes/game.routes.ts
@@ -2,13 +2,16 @@ import { Router } from "express";
 import {GameController} from '../controllers/game.controller'
 import { isAdmin } from "../middlewares/user.middleware";
 import { isAuthenticate } from "../middlewares/auth.middleware";
+import { RoomController } from "@/controllers/room.controller";
 
 const router = Router()
 
-router.get('/',isAuthenticate, isAdmin, GameController.getAll)
-router.get('/:id',isAuthenticate, isAdmin, GameController.getById)
+router.get('/', GameController.getAll)
+router.get('/:id', GameController.getById)
 router.post('/',isAuthenticate,isAdmin, GameController.create)
 router.put('/:id',isAuthenticate, isAdmin, GameController.update)
 router.delete('/:id',isAuthenticate, isAdmin, GameController.delete)
+
+router.get('/room', RoomController.getAll)
 
 export default router
