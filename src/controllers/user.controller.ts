79802e5390fe30c420f6d56da34b6c197f8d92f2
JorgeAlleres <alleresjorge10@gmail.com>
2025-02-18 22:27:32 +0100

    Añado GetById y Delete

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,3 +1,4 @@
+import { HttpException } from "@/exceptions/httpException";
 import { UserService } from "../services/user.service";
 import { Response, Request, NextFunction } from 'express'
 
@@ -11,6 +12,16 @@ export class UserController {
             next(error)
         }
     }
+    static async getById(req: Request, res: Response, next: NextFunction) {
+            try {
+                const id = parseInt(req.params.id);
+                if (isNaN(id)) throw new HttpException(400, "Invalid user ID");
+                const user = await UserService.getById(id);
+                res.status(200).json(user)            
+            } catch (error) {
+                next(error);
+            }
+        }
     static async usersList(req: Request, res: Response, next: NextFunction) {
         try {
             const users = await UserService.getAll()
@@ -19,4 +30,28 @@ export class UserController {
             next(error)
         }
     }
+    static async update(req: Request, res: Response, next: NextFunction) {
+        try {
+            const userUpdate = req.body
+            const id = Number.parseInt(req.params.id)
+            if (isNaN(id)) throw new HttpException(400, "Invalid user ID");
+
+            const updatedUser = await UserService.update(id, userUpdate)
+
+            res.status(201).json({ message: 'user updated successfully', updatedUser })
+        } catch (error) {
+            next(error)
+        }
+    }
+    static async delete(req: Request, res: Response, next: NextFunction) {
+            try {
+                const id = Number.parseInt(req.params.id)
+                if (isNaN(id)) throw new HttpException(400, "Invalid user ID");
+    
+                const userDeleted = await UserService.delete(id)
+                res.status(201).json({ message: 'User deleted successfully', userDeleted })
+            } catch (error) {
+                next(error)
+            }
+        }
 }
