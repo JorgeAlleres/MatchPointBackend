ad378d8edce5d811ff13aa7fcd2b7e9b0380177a
JorgeAlleres <alleresjorge10@gmail.com>
2025-02-18 22:28:25 +0100

    Update y delete de users

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -6,6 +6,9 @@ import { isAdmin } from "../middlewares/user.middleware";
 const router = Router()
 
 router.get('/', isAuthenticate, isAdmin, UserController.usersList)
+router.get('/:id', UserController.getById)
+router.put('/:id'/*, isAuthenticate, isAdmin*/, UserController.update)
+router.delete('/:id'/*, isAuthenticate, isAdmin*/, UserController.delete)
 router.get('/profile', isAuthenticate, UserController.profile)
 
 export default router
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,22 +1,38 @@
+import { User } from "prisma/prisma-client"
 import { prisma } from "../database/database"
 import { HttpException } from "../exceptions/httpException"
 
 export class UserService {
 
-    static async getById(id:number) {
-        const findUser = await prisma.user.findUnique({where:{id}})
-        if(!findUser) throw new HttpException(404, 'User not found')
+    static async getById(id: number) {
+        const findUser = await prisma.user.findUnique({ where: {id}})
+        if (!findUser) throw new HttpException(404, 'User not found')
         return findUser
     }
 
-    static async getByEmail(email:string) {
-        const findUser = await prisma.user.findUnique({where:{email}, omit:{password:true}})
-        if(!findUser) throw new HttpException(404, 'User not found')
+    static async getByEmail(email: string) {
+        const findUser = await prisma.user.findUnique({ where: { email }, omit: { password: true } })
+        if (!findUser) throw new HttpException(404, 'User not found')
         return findUser
     }
 
     static async getAll() {
-        const findUsers = await prisma.user.findMany({omit:{password:true}})
+        const findUsers = await prisma.user.findMany({ omit: { password: true } })
         return findUsers
+    }
+    static async update(idUser: number, user: User) {
+        const userUpdate = await prisma.user.update({
+            where: { id: idUser },
+            data: { ...user }
+        })
+        if (!userUpdate) throw new HttpException(409, `UserID ${idUser} doesnt exists`)
+        return userUpdate
+    }
+    static async delete(id: number) {
+        try {
+            return await prisma.user.delete({ where: { id } });
+        } catch (error) {
+            throw new HttpException(404, "User not found");
+        }
     }
 }
