2b4cd1223819471d2d72965d38a15cf152c60806
JorgeAlleres <alleresjorge10@gmail.com>
2025-02-26 17:21:03 +0100

    Fallos con CORS

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(cookieParser())
 app.use(express.json())
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://matchpointbackend.onrender.com', '*'],
+    origin: ['https://matchpointbackend.onrender.com', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
