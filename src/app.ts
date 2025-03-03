2697be670b21c99c5d89f203bf306a9db2a7bbcd
JorgeAlleres <alleresjorge10@gmail.com>
2025-03-03 19:53:21 +0100

    Correcciones para CORS

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use(cookieParser())
 app.use(express.json())
 
 app.use(cors({
-    origin: ['https://matchpointfrontend.onrender.com', 'http://localhost:5173'],
+    origin: ['https://matchpointfrontend.onrender.com', 'http://localhost:5173', 'http://localhost:4173', '*'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
